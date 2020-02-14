5c129743a6d718efd51659f8816c804170dba499
Steve Archuleta <stevearchuleta@me.com>
2020-02-14 13:53:53 -0800

    fix mongoDB connection with useUnifiedTopology flag; adjust App.js heading

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
-const express = require("express");
+const express = require('express');
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const mongoose = require('mongoose');
+// const routes = require("./routes");
+const cors = require('cors');
+
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json()); //allows me to parse JSON becasue server will send and receive JSON
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
+const uri = process.env.ATLAS_URI;
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  uri,
   {
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true
   }
-);
+  );
+mongoose.connection.once('open', () => {
+    console.log("MongoDB database connection has been established successfully.");
+  })
 
 // Start the API server
-app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+app.listen(PORT, () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './books.png';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src= {logo} className="App-logo" alt="logo" />
-        <p>
-          React Google Books Search App
-        </p>
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App-container">
+        <header className="App-header">
+          <img src= {logo} className="App-logo" alt="logo" />
+          <h2>
+          Google Books Search
+          </h2>
+          <p>
+          A React App By Steve Archuleta
+          </p>
+          
+        </header>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
